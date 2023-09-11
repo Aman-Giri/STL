e7366ee8ee35e9cc6c42ef41202cea920b603ca0
Aman Giri <amangirikg@gmail.com>
2023-09-11 22:51:27 +0530

    STL completed at day 2

---
+++ Map.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+int main(){
+    map<int,string> m;
+    m[1]="Aman Giri";
+    m[13]="Karan Sanwal";
+    m[2]="love babbar";
+
+    m.insert({5,"Sunny Yadav"});
+
+    cout<<"Printing m before using the erase function: "<<endl;
+    for(auto i:m){
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+
+    cout<<"Finding the 13->"<<m.count(13)<<endl;
+
+    m.erase(13);
+    cout<<"Printing m after using the erase function: "<<endl;
+    for(auto i:m){
+        cout<<i.first<<" "<<i.second<<endl;
+    }
+    cout<<endl;
+
+    auto it = m.find(2);
+    for(auto i=it;i!=m.end();i++){
+        cout<<(*i).first<<" "<<(*i).second<<endl;
+    }
+}
